6b7ed162b20a0e427d6a7ec58fc834b39dbeb5e2
RajKumar0696 <mrajkumar0696@gmail.com>
2023-04-11 09:55:39 +0530

    add chrome test for parallel testing

---
+++ Docker/Chrometest1.py
@@ -0,0 +1,12 @@
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdriver
+
+class ChromeTest:
+    def testcase(self):
+
+        driver =webdriver.Remote('http://selenium: 4444/wd/hub',
+                                  desired_capabilities=DesiredCapabilities.CHROME)
+        driver.get('https://python.org')
+        driver.save_screenshot('screenshot.png')
+        driver.close()
